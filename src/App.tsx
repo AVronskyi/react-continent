8e96a538a779df1fcc2ca3b7731d54877e903a3b
AVronskyi <vronskyi.a@gmail.com>
2021-11-11 22:05:56 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import continents from './components/continents';
 
-function App() {
+// const ListCoutri: React.FC<Country> = ({ countries }) => {
+//   let languages = null;
+//   if (countries.languages && countries.languages.length) {
+//     languages = (
+//       <ul>
+//         {countries.languages.map(l => (
+//           <Languages item={i} key={languages.name} />
+//         ))}
+//       </ul>
+//     );
+//   }
+//   return (
+//     <li>
+//       {countries.name}
+//       {languages}
+//     </li>
+//   )
+// };
+
+// const Languages: React.FC<Language> = ({ languages }) => {
+  
+//   return (
+//     <li>
+//       {languages.name}
+//     </li>
+//   )
+// };
+
+const App: React.FC = () => {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {continents.map(countries => (
+          <li
+            
+          >
+
+          </li>
+        ))}
+      </ul>
     </div>
-  );
-}
+  )
+};
 
 export default App;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="react-scripts" />
+
+interface Continents {
+  name: string;
+  countries: Country[];
+}
+
+interface Country {
+  name: string;
+  languages: Language[];
+}
+
+interface Language {
+  name: string
+}
